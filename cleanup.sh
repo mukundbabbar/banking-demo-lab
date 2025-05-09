596e460582cb387459044dfeadaf4ee9dc36cd5d
Mukund Babbar <mbabbar@splunk.com>
2025-05-09 11:48:18 +1000

    Add cleanup

---
+++ cleanup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "Deleting ConfigMap and Secret..."
+
+kubectl delete configmap appd-config --ignore-not-found
+kubectl delete secret appd-secret --ignore-not-found
+kubectl delete -f k8s
+echo "✅ Cleanup complete."
